1d39d0efa6e8cd43de96a939eca8fd70349c81b4
Mike Repec <mikerepec@gmail.com>
2022-12-13 23:43:28 +0100

    Init Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -7,9 +7,13 @@ fun main() {
         return input.size
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+
+    val part1Result = part1(testInput)
+    check(part1Result == 1) { "Part 1: Expected 1, actual $part1Result" }
+
+    val part2Result = part2(testInput)
+    check(part2Result == 2) { "Part 2: Expected 2, actual $part2Result" }
 
     val input = readInput("Day01")
     part1(input).println()
