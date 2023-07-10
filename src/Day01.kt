0da465c7db09560c65290a36c8bc16408d5dc365
Mike Repec <mikerepec@gmail.com>
2023-07-11 00:03:02 +0200

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,19 +1,28 @@
 fun main() {
+    fun sumTop(input: List<String>, n: Int): Int {
+        return input.blocks { backpack -> backpack.sumOf { it.toInt() } }
+            .sorted()
+            .takeLast(n)
+            .sum()
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return sumTop(input, 1)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return sumTop(input, 3)
     }
 
     val testInput = readInput("Day01_test")
 
     val part1Result = part1(testInput)
-    check(part1Result == 1) { "Part 1: Expected 1, actual $part1Result" }
+    val expected1 = 24000
+    check(part1Result == expected1) { "Part 1: Expected $expected1, actual $part1Result" }
 
     val part2Result = part2(testInput)
-    check(part2Result == 2) { "Part 2: Expected 2, actual $part2Result" }
+    val expected2 = 45000
+    check(part2Result == expected2) { "Part 2: Expected $expected2, actual $part2Result" }
 
     val input = readInput("Day01")
     part1(input).println()
--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,18 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+/**
+ * Split [List] into blank-string-separated blocks, and perform a [transform] operation on each,
+ * returning the resulting [List] of transformed elements of type [R].
+ */
+fun <R> List<String>.blocks(transform: (List<String>) -> R): List<R> = buildList {
+    this@blocks.fold(listOf<String>()) { current, line ->
+        if (line.isNotBlank()) {
+            current + line
+        } else {
+            add(transform(current))
+            mutableListOf()
+        }
+    }.takeIf { it.isNotEmpty() }?.let { add(transform(it)) }
+}
